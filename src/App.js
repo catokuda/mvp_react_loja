46bde91a72b7498f7dffce3f70f2ac812e6f5105
Camila Tokuda <106205008+catokuda@users.noreply.github.com>
2024-12-06 14:56:53 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,55 +2,8 @@ import React, { useState } from 'react';
 import products from './data/products.json';
 import ProductCard from './components/ProductCard';
 import Modal from './components/Modal';
+import Header from './componentes/Header';
 
-// Componente Header
-const Header = () => {
-  return (
-    <header style={headerStyles}>
-      <div style={logoStyles}>
-        <img
-          src="./logo.png"
-          alt="Logo da Loja"
-          style={{ width: '150px', height: 'auto' }}
-        />
-      </div>
-      <nav style={navStyles}>
-        
-        {/* Links de ancoragem que rolam suavemente para as seções */}
-        <a href="#introduction" style={linkStyles}>Início</a>
-        <a href="#shop" style={linkStyles}>Loja</a>
-        <a href="#contato" style={linkStyles}>Contato</a>
-      </nav>
-    </header>
-  );
-};
-
-const headerStyles = {
-  display: 'flex',
-  justifyContent: 'space-between',
-  alignItems: 'center',
-  padding: '20px',
-  backgroundColor: '#fff',
-  boxShadow: '0 2px 5px rgba(0, 0, 0, 0.1)',
-};
-
-const logoStyles = {
-  display: 'flex',
-  alignItems: 'center',
-};
-
-const navStyles = {
-  display: 'flex',
-  gap: '20px',
-};
-
-const linkStyles = {
-  textDecoration: 'none',
-  color: '#333',
-  fontSize: '16px',
-  fontWeight: 'bold',
-  transition: 'color 0.3s ease',
-};
 
 // Componente Principal (App.js)
 const App = () => {
